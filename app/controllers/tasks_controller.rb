f584a5a4aaf2808255ed1ab4892e3b654ab6ec8b
pac.a.shimizu <pac.a.shimizu@gmail.com>
2022-05-04 13:18:58 +0000

    correct user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  #before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @pagy, @tasks = pagy(Task.order(id: :desc), items: 5)
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc), items: 5)
   end
   
   def show
@@ -15,13 +16,13 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "タスクが正常に登録されました"
       redirect_to @task
     else
-      flash.now[:danger] = "タスクが登録できませんでした"
+      flash.now[:danger] = "タスクを登録できませんでした"
       render :new
     end
   end
@@ -37,7 +38,7 @@ class TasksController < ApplicationController
       flash[:success] = "タスクは正常に更新されました"
       redirect_to @task
     else
-      flash.now[:danger] = "タスクが更新できませんでした"
+      flash.now[:danger] = "タスクを更新できませんでした"
       render :edit
     end
   end
@@ -46,19 +47,26 @@ class TasksController < ApplicationController
     
     @task.destroy
     
-    flash[:success] = "タスクが削除されました"
+    flash[:success] = "タスクを削除しました"
     redirect_to tasks_url
   end
   
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  #def set_task
+    #@task = current_user.tasks.find_by(id: params[:id])
+  #end
   
   #Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
